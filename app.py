dc252898b6f798fd88d6f93d2c7f0bd0a2befe9a
koldoarretxea <koldo.arretxea@gmail.com>
2023-07-21 11:37:00 +0200

    Initial commit

---
+++ app.py
@@ -0,0 +1,144 @@
+from flask import Flask, request, jsonify
+from flask_cors import CORS
+import mysql.connector
+
+app = Flask(__name__)
+CORS(app)
+
+# Configura la conexión a la base de datos MySQL
+db = mysql.connector.connect(
+    host='127.0.0.1',
+    port='3306',
+    user='root',
+    password='estibaliZ1.',
+    database='fullstack_bottega'
+)
+
+# Ruta para el registro de usuarios
+@app.route('/register', methods=['POST'])
+def register():
+    data = request.get_json()
+    email = data['email']
+    
+    # Verifica si el usuario ya está registrado
+    cursor = db.cursor()
+    cursor.execute("SELECT * FROM users WHERE email_users=%s", (email,))
+    result = cursor.fetchone()
+    
+    if result is not None:
+        return jsonify({'message': 'El usuario ya está registrado'}), 400
+    
+    # Inserta el nuevo usuario en la base de datos
+    cursor.execute("INSERT INTO users (email_users) VALUES (%s)", (email,))
+    db.commit()
+    
+    return jsonify({'message': 'Registro exitoso'}), 200
+
+# Ruta para la autenticación de usuarios
+@app.route('/login', methods=['POST'])
+def login():
+    data = request.get_json()
+    email = data['email']
+    
+    # Verifica si el usuario existe en la base de datos
+    cursor = db.cursor()
+    cursor.execute("SELECT * FROM users WHERE email_users=%s", (email,))
+    result = cursor.fetchone()
+    
+    if result is None:
+        return jsonify({'message': 'Usuario no encontrado'}), 404
+    
+    return jsonify({'message': 'Inicio de sesión exitoso'}), 200
+
+# Ruta para el cierre de sesión
+@app.route('/logout', methods=['GET'])
+def logout():
+    # Realiza las acciones necesarias para cerrar sesión, como limpiar las cookies o el estado de autenticación
+    return jsonify({'message': 'Sesión cerrada exitosamente'}), 200
+
+# Ruta para verificar el estado de autenticación
+@app.route('/check-authentication', methods=['GET'])
+def check_authentication():
+    # Aquí puedes realizar la lógica para verificar si el usuario está autenticado o no
+    # Puedes usar cookies, tokens u otros métodos de autenticación según tu implementación
+    # En este ejemplo, simplemente devolvemos un estado de autenticación aleatorio para demostración
+    is_authenticated = True  # Aquí debes implementar tu propia lógica de autenticación
+    
+    return jsonify({'isLoggedIn': is_authenticated}), 200
+
+# Rutas para las operaciones CRUD
+@app.route('/items', methods=['GET', 'POST'])
+def handle_items():
+    if request.method == 'GET':
+        # Consulta SQL para obtener todos los registros de la tabla
+        query = 'SELECT * FROM items'
+
+        # Ejecutar la consulta
+        cursor = db.cursor()
+        cursor.execute(query)
+
+        # Obtener los resultados y construir la lista de elementos
+        items = []
+        for item in cursor.fetchall():
+            item_data = {
+                'id': item[0],
+                'name': item[1]
+            }
+            items.append(item_data)
+
+        return jsonify(items)
+
+    elif request.method == 'POST':
+        item = request.json.get('name')
+        if item:
+            # Consulta SQL para insertar un nuevo registro en la tabla
+            query = 'INSERT INTO items (name) VALUES (%s)'
+
+            # Datos del nuevo elemento
+            item_data = (item,)
+
+            # Ejecutar la consulta
+            cursor = db.cursor()
+            cursor.execute(query, item_data)
+            db.commit()
+
+            return jsonify({'message': 'Item added successfully'})
+        else:
+            return jsonify({'error': 'Invalid item'})
+
+@app.route('/items/<int:index>', methods=['DELETE', 'PUT'])
+def handle_item_by_index(index):
+    if request.method == 'DELETE':
+        # Consulta SQL para eliminar un registro de la tabla
+        query = 'DELETE FROM items WHERE id = %s'
+
+        # Datos del índice del elemento a eliminar
+        item_index = (index,)
+
+        # Ejecutar la consulta
+        cursor = db.cursor()
+        cursor.execute(query, item_index)
+        db.commit()
+
+        return jsonify({'message': 'Item deleted successfully'})
+
+    elif request.method == 'PUT':
+        new_name = request.json.get('name')
+        if new_name:
+            # Consulta SQL para actualizar un registro de la tabla
+            query = 'UPDATE items SET name = %s WHERE id = %s'
+
+            # Datos del nuevo nombre y el índice del elemento a editar
+            item_data = (new_name, index)
+
+            # Ejecutar la consulta
+            cursor = db.cursor()
+            cursor.execute(query, item_data)
+            db.commit()
+
+            return jsonify({'message': 'Item edited successfully'})
+        else:
+            return jsonify({'error': 'Invalid item'})
+
+if __name__ == '__main__':
+    app.run(debug=True)
--- src/App.js
+++ src/App.js
@@ -1,25 +1,142 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
 
-function App() {
+import './styles/App.scss';
+
+const App = () => {
+  const [email, setEmail] = useState('');
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
+  const [inputValue, setInputValue] = useState('');
+  const [items, setItems] = useState([]);
+  const [editMode, setEditMode] = useState(false);
+  const [editItemId, setEditItemId] = useState(null);
+
+  useEffect(() => {
+    // Verificar el estado de autenticación al cargar el componente
+/*     const checkAuthentication = async () => {
+      try {
+        const response = await axios.get('http://localhost:5000/check-authentication');
+        setIsLoggedIn(response.data.isLoggedIn);
+        if (response.data.isLoggedIn) {
+          fetchItems();
+        }
+      } catch (error) {
+        console.error('Error al verificar la autenticación:', error);
+      }
+    };
+
+    checkAuthentication(); */
+  }, []);
+
+  const fetchItems = async () => {
+    try {
+      const response = await axios.get('http://localhost:5000/items');
+      setItems(response.data);
+    } catch (error) {
+      console.error('Error fetching items:', error);
+    }
+  };
+
+  const handleInputChange = (e) => {
+    setInputValue(e.target.value);
+  };
+
+  const handleAddItem = async () => {
+    if (inputValue) {
+      try {
+        await axios.post('http://localhost:5000/items', { name: inputValue });
+        setInputValue('');
+        fetchItems();
+      } catch (error) {
+        console.error('Error adding item:', error);
+      }
+    }
+  };
+
+  const handleEditItem = (id) => {
+    const newName = prompt('Enter the new name');
+    if (newName) {
+      try {
+        axios.put(`http://localhost:5000/items/${id}`, { name: newName });
+        fetchItems();
+      } catch (error) {
+        console.error('Error editing item:', error);
+      }
+    }
+  };
+
+  const handleDeleteItem = async (id) => {
+    try {
+      await axios.delete(`http://localhost:5000/items/${id}`);
+      fetchItems();
+    } catch (error) {
+      console.error('Error deleting item:', error);
+    }
+  };
+
+  const handleLogin = async () => {
+    try {
+      const response = await axios.post('http://localhost:5000/login', { email: email });
+      console.log(response.data.message);
+      setIsLoggedIn(true); // Establecer el estado isLoggedIn a true cuando se inicia sesión correctamente
+      fetchItems(); // Cargar los elementos desde el servidor después de iniciar sesión
+    } catch (error) {
+      console.error(error.response.data.message);
+    }
+  };
+
+  const handleLogout = async () => {
+    try {
+      const response = await axios.get('http://localhost:5000/logout');
+      console.log(response.data.message);
+      setIsLoggedIn(false); // Establecer el estado isLoggedIn a false cuando se cierra sesión correctamente
+      setItems([]); // Borrar la lista de elementos después de cerrar sesión
+    } catch (error) {
+      console.error(error.response.data.message);
+    }
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='container'>
+      {isLoggedIn ? (
+        <div>
+          <p>Bienvenido {email}! Estás autenticado.</p>
+          <button onClick={handleLogout}>Cerrar sesión</button>
+
+          <div className='subcontainer'>
+            <input
+              type="text"
+              value={inputValue}
+              onChange={handleInputChange}
+              placeholder="Enter an item"
+            />
+            <button onClick={handleAddItem}>{editMode ? 'Save' : 'Add'}</button>
+
+            <button onClick={fetchItems}>Read from Database</button>
+          </div>
+          <ul>
+            {items.map((item) => (
+              <li key={item.id}>
+                {item.id} - {item.name}
+                <button className='button-edit' onClick={() => handleEditItem(item.id)}>Edit</button>
+                <button onClick={() => handleDeleteItem(item.id)}>Delete</button>
+              </li>
+            ))}
+          </ul>
+        </div>
+      ) : (
+        <div className='login'>
+          <input
+            type="text"
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+            placeholder="Correo electrónico"
+          />
+          <button onClick={handleLogin}>Iniciar sesión</button>
+        </div>
+      )}
     </div>
   );
-}
+};
 
 export default App;
